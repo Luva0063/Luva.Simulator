5c7e5328eb2fd7dbfd2183730bb711066cd662ce
Luva0063 <154241253+Luva0063@users.noreply.github.com>
2025-01-23 19:14:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-<body>
 
-<h2>What Can JavaScript Do?</h2>
-
-<p>JavaScript can change HTML attribute values.</p>
-
-<p>In this case JavaScript changes the value of the src (source) attribute of an image.</p>
-
-<button onclick="document.getElementById('my image').src='Untitled design (1).png'">open parade doors</button>
-
-<img id="Untitled design.png" src="Untitled design.png" style="width:100px">
-
-<button onclick="document.getElementById('My image').src='Untitled design.png'">close parade doors </button>
-
-</body>
